35a135d425da243ee1af5824013cc69a08731c7d
ghaith99 <ghaithix@hotmail.com>
2015-12-06 06:16:20 +0300

    Update manipulator.js

--- data/manipulator.js
+++ data/manipulator.js
@@ -1,37 +1,28 @@
-self.on("context", function (node) {
-  let videos = Array.prototype.slice.call(document.getElementsByTagName("video"));
-  /*
-    TODO
-    Content scripts can't access content hosted in an iframe, if that content is served from a different domain,
-    so we can't access videos in iframes, which is a problem with iframe embeded videos
-    https://addons.mozilla.org/en-US/developers/docs/sdk/latest/dev-guide/guides/content-scripts/cross-domain.html
+//on Keydown listener
+function handle(e){
 
-    we could try whitelisting domains under the "cross-domain-content" key for various video sites
-  */
-  videos = videos.filter(function (video) {
-    return !video.paused;
-  });
-  if (videos.length === 1) {
-    self.postMessage(videos[0].playbackRate + "X");
-    return true;
-  } else {
-    self.postMessage('reset');
-    return false;
+  if(!document.getElementsByTagName("video")[0].paused){ //err if vid paused and change
+    
+    if(e.keyCode == 107){
+      document.getElementsByTagName("video")[0].playbackRate += 1;
+    }
+    if(e.keyCode == 109){
+      if(document.getElementsByTagName("video")[0].playbackRate != 1){
+        document.getElementsByTagName("video")[0].playbackRate -= 1;
+      }
+    }   
   }
-});
 
+}
+
+window.addEventListener("keydown", handle, true);
+
+//on Menu Click listener
+//menu items, change labels in lib/main  
 self.on("click", function (node, data) {
-  let videos = Array.prototype.slice.call(document.getElementsByTagName("video"));
-  videos = videos.filter(function (video) {
-    return !video.paused;
-  });
-  if (data.endsWith('X')) {
-    //fixed value, strip X
-    let rate = data.slice(0, -1);
-    rate = parseFloat(rate)
-    videos[0].playbackRate = rate;
+
+  if (data.endsWith('labelName')) {
+
     return;
   }
-  data = parseFloat(data);
-  videos[0].playbackRate += data;
 });
